7dc27e0cc4cfdf1049a48bd64a59428b722eec03
Prajwal Rudrakshi <pgr19990109@gmail.com>
2020-10-24 21:58:35 +0530

    login as fragment. Google sign in complete

--- app/src/main/java/com/example/us0/MainActivity.kt
+++ app/src/main/java/com/example/us0/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.us0
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.us0.databinding.ActivityMainBinding
+
 
 class MainActivity: AppCompatActivity() {
 
@@ -11,5 +13,8 @@ class MainActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
--- app/src/main/java/com/example/us0/ui/login/LoginFragment.kt
+++ app/src/main/java/com/example/us0/ui/login/LoginFragment.kt
@@ -1,40 +1,71 @@
 package com.example.us0.ui.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
 import android.widget.Button
-import androidx.databinding.DataBindingUtil
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.annotation.StringRes
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.us0.R
 import com.example.us0.databinding.FragmentLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
 //import com.firebase.ui.auth.AuthUI
 //import com.firebase.ui.auth.IdpResponse
 //import com.google.firebase.auth.FirebaseAuth
 
 
-class LoginFragment : Fragment() {
+class LoginFragment : Fragment(),View.OnClickListener {
 
     private lateinit var loginViewModel: LoginViewModel
-
+    private lateinit var auth: FirebaseAuth
+    private lateinit var mgoogleSignInClient: GoogleSignInClient
+    private lateinit var binding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding:FragmentLoginBinding=DataBindingUtil.inflate(inflater,R.layout.fragment_login,container,false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
 
-            return binding.root
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val appContext = context?.applicationContext ?: return binding.root
+        mgoogleSignInClient = GoogleSignIn.getClient(appContext, gso)
+
+        auth = Firebase.auth
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        updateUI(currentUser)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -47,7 +78,7 @@ class LoginFragment : Fragment() {
         val loginButton = view.findViewById<Button>(R.id.login)
         val loadingProgressBar = view.findViewById<ProgressBar>(R.id.loading)
 
-        loginViewModel.loginFormState.observe(this,
+        loginViewModel.loginFormState.observe(viewLifecycleOwner,
             Observer { loginFormState ->
                 if (loginFormState == null) {
                     return@Observer
@@ -61,7 +92,7 @@ class LoginFragment : Fragment() {
                 }
             })
 
-        loginViewModel.loginResult.observe(this,
+        loginViewModel.loginResult.observe(viewLifecycleOwner,
             Observer { loginResult ->
                 loginResult ?: return@Observer
                 loadingProgressBar.visibility = View.GONE
@@ -108,6 +139,71 @@ class LoginFragment : Fragment() {
                 passwordEditText.text.toString()
             )
         }
+
+        binding.signInButton.setOnClickListener(this)
+
+    }
+
+    private fun signIn() {
+        val signInIntent: Intent = mgoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // ...
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    // ...
+                    view?.let { Snackbar.make(it, "Authentication Failed.", Snackbar.LENGTH_SHORT).show() }
+                    updateUI(null)
+                }
+
+                // ...
+            }
+    }
+
+
+    private fun updateUI(user: FirebaseUser?) {
+        //hideProgressBar()
+        if (user != null) {
+            // binding.status.text = getString(R.string.google_status_fmt, user.email)
+            // binding.detail.text = getString(R.string.firebase_status_fmt, user.uid)
+
+            binding.signInButton.visibility = View.GONE
+
+        } else {
+            // binding.status.setText(R.string.signed_out)
+            //binding.detail.text = null
+
+            binding.signInButton.visibility = View.VISIBLE
+
+        }
     }
 
     private fun updateUiWithUser(model: LoggedInUserView) {
@@ -120,5 +216,16 @@ class LoginFragment : Fragment() {
     private fun showLoginFailed(@StringRes errorString: Int) {
         val appContext = context?.applicationContext ?: return
         Toast.makeText(appContext, errorString, Toast.LENGTH_LONG).show()
+    }
+
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 4564
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.sign_in_button -> signIn()
+        }
     }
 }
